3203bcad77c45ae6d0548cf6abaeb9f7b2c7b1cc
ernanijmaximize <132609996+ernanijmaximize@users.noreply.github.com>
2024-07-25 17:26:21 -0300

    Terminando a estlização do modal

--- src/app/modules/portfolio/components/dialog/dialog-projects/dialog-projects.component.ts
+++ src/app/modules/portfolio/components/dialog/dialog-projects/dialog-projects.component.ts
@@ -1,12 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, Inject, OnInit, signal } from '@angular/core';
+import { IProject } from '../../../interface/IProject.interface';
+import { MAT_DIALOG_DATA, MatDialogModule, MatDialogRef } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-dialog-projects',
   standalone: true,
-  imports: [],
+  imports: [MatDialogModule],
   templateUrl: './dialog-projects.component.html',
   styleUrl: './dialog-projects.component.scss'
 })
-export class DialogProjectsComponent {
+export class DialogProjectsComponent implements OnInit{
+  constructor(
+    private _matDialogRef: MatDialogRef<DialogProjectsComponent>,
+    @Inject(MAT_DIALOG_DATA) private _data: IProject
+  ){
 
+  }
+
+  public getData = signal<IProject | null>(null);
+
+
+  ngOnInit(): void {
+    this.getData.set(this._data);
+  }
+
+  public closeModal(){
+    return this._matDialogRef.close();
+  }
 }
